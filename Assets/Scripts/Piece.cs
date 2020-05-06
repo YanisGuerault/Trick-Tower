0025541a79d53e8788e425243fb6310aa75532b4
Forley <yanisguerault@gmail.com>
2020-05-06 15:05:26 +0200

    [ADD] Begin of gameplay structure

---
+++ Assets/Scripts/Piece.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece : MonoBehaviour
+{
+    float gravityScaleBase;
+    float lastPositionY;
+    bool grounded;
+
+    private void Start()
+    {
+        this.gravityScaleBase = this.GetComponent<Rigidbody2D>().gravityScale;
+    }
+    private void FixedUpdate()
+    {
+        if (!grounded)
+        {
+
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                this.GetComponent<Rigidbody2D>().gravityScale += .1f;
+            }
+            else
+            {
+                this.GetComponent<Rigidbody2D>().gravityScale = gravityScaleBase;
+            }
+
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.position = new Vector3(transform.position.x + .1f, transform.position.y, transform.position.z);
+            }
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.position = new Vector3(transform.position.x - .1f, transform.position.y, transform.position.z);
+            }
+
+
+            if ((this.GetComponent<Transform>().position.y) < -50)
+            {
+                // Remove bloc
+                // Retire one life
+                // 
+            }
+
+            lastPositionY = (this.GetComponent<Transform>().position.y);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!grounded && (collision.gameObject.tag == "Ground" || collision.gameObject.tag == "Piece"))
+        {
+            grounded = true;
+            GameObject.FindGameObjectWithTag("Spawn Box").GetComponent<SpawnBox>().SpawnNewBox();
+        }
+    }
+
+}
---
+++ Assets/Scripts/SpawnBox.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SpawnBox : MonoBehaviour
+{
+
+    public GameObject[] boxList;
+
+    void Start()
+    {
+        SpawnNewBox();
+    }
+
+    public void SpawnNewBox()
+    {
+        int i = Random.Range(0, boxList.Length);
+        Instantiate(boxList[i], transform.position, Quaternion.identity);
+    }
+}
