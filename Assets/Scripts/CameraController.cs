7439afb2536281a1a707b204a3bbd5a6f7b25eba
Forley <yanisguerault@gmail.com>
2020-05-13 16:47:04 +0200

    Ajout mode 2 joueurs + système de vie

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public Transform spawner;
+    Transform[] spawner;
     public Transform ground;
     public SpawnBox spawn;
     private float delta;
@@ -15,6 +15,13 @@ public class CameraController : MonoBehaviour
     void Start()
     {
         firstPosition = transform.position;
+        spawner = new Transform[10];
+        int i = 0;
+        foreach(GameObject go in GameObject.FindGameObjectsWithTag("Spawn Box"))
+        {
+            spawner[i] = go.transform;
+            i++;
+        }
     }
 
     // Update is called once per frame
@@ -22,33 +29,49 @@ public class CameraController : MonoBehaviour
     {
         if (findHighestObject("Piece") != null)
         {
-            float high = findHighestObject("Piece").transform.position.y;
 
-            Debug.Log("High : " + high + " | Camera : " + transform.position.y);
+            //Debug.Log("High : " + high + " | Camera : " + transform.position.y);
 
-            if (high > transform.position.y)
+            float objectToFollow = findHighestObject("Piece").transform.position.y;
+
+            /*if(spawn.getLastPiece().transform.position.y < transform.position.y)
             {
-                delta = high+1 - transform.position.y;
-                transform.transform.position = Vector3.SmoothDamp(transform.transform.position, new Vector3(transform.position.x, high + 1, transform.position.z), ref velocity, 0.4F);
+                objectToFollow = spawn.getLastPiece().transform.position.y;
+            }
+            else
+            {
+                objectToFollow = findHighestObject("Piece").transform.position.y;
+            }*/
+
+            if (objectToFollow > transform.position.y)
+            {
+                delta = objectToFollow + 1 - transform.position.y;
+                transform.transform.position = Vector3.SmoothDamp(transform.transform.position, new Vector3(transform.position.x, objectToFollow + 1, transform.position.z), ref velocity, 0.4F);
                 //>.transform.position = new Vector3(>.position, new Vector3(>.position.x, high+1, >.position.z);
-                spawner.transform.position = Vector3.SmoothDamp(spawner.transform.position, new Vector3(spawner.position.x, spawner.position.y + delta, spawner.position.z), ref velocity, 0.4F);
+                foreach (Transform spawn in spawner)
+                {
+                    spawn.position = Vector3.SmoothDamp(spawn.position, new Vector3(spawn.position.x, spawn.position.y + delta, spawn.position.z), ref velocity, 0.4F);
+                }
                 //spawner.transform.position = new Vector3(spawner.position.x, spawner.position.y + delta, spawner.position.z);
             }
 
-            if (high < transform.position.y)
+            if (objectToFollow < transform.position.y && transform.position.y > firstPosition.y)
             {
-                delta = transform.position.y - high;
+                delta = transform.position.y - objectToFollow;
                 Transform spawnPosition = spawn.getLastPiece().transform;
                 spawnPosition.position = Vector3.SmoothDamp(spawnPosition.position, new Vector3(spawnPosition.position.x, spawnPosition.position.y - delta, spawnPosition.position.z), ref velocity, 0.4F);
-                transform.position = Vector3.SmoothDamp(transform.position, new Vector3(transform.position.x, high, transform.position.z), ref velocity, 0.4F);
-                spawner.transform.position = Vector3.SmoothDamp(spawner.position, new Vector3(spawner.position.x, spawner.position.y - delta, spawner.position.z), ref velocity, 0.4F);
+                transform.position = Vector3.SmoothDamp(transform.position, new Vector3(transform.position.x, objectToFollow, transform.position.z), ref velocity, 0.4F);
+                foreach (Transform spawn in spawner)
+                {
+                    spawn.position = Vector3.SmoothDamp(spawn.position, new Vector3(spawn.position.x, spawn.position.y - delta, spawn.position.z), ref velocity, 0.4F);
+                }
                 /*spawnPosition.position = new Vector3(spawnPosition.position.x, spawnPosition.position.y - delta, spawnPosition.position.z);
                 >.transform.position = new Vector3(>.position.x, high, >.position.z);
                 spawner.transform.position = new Vector3(spawner.position.x, spawner.position.y - delta, spawner.position.z);*/
             }
         }
     }
-    
+
     GameObject findHighestObject(string tag)
     {
         GameObject[] pieceList = GameObject.FindGameObjectsWithTag(tag);
